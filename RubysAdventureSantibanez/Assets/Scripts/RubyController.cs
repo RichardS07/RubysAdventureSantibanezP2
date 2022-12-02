9890c1d54d2f5cff44576744ba82903b4429d618
RichardS07 <curesma0731@gmail.com>
2022-12-02 09:57:30 -0800

    12/02/22
    Resized the map to fit into a 90 by 90 area. Fixed ruby's jittering and gave her health.

--- RubysAdventureSantibanez/Assets/Scripts/RubyController.cs
+++ RubysAdventureSantibanez/Assets/Scripts/RubyController.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public int maxHealth = 5;
+    int currentHealth;
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -11,6 +14,8 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -21,12 +26,21 @@ public class RubyController : MonoBehaviour
         vertical = Input.GetAxis("Vertical");
     }
 
-    private void FixedUpdate()
+    void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
         rigidbody2d.MovePosition(position);
+    } 
+
+    void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
-}   
+        
+}  
+
+
