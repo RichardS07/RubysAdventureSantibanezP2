99dac2a709f80c1b9531a8acb8d76c8a1d9951cb
RichardS07 <curesma0731@gmail.com>
2022-12-12 09:57:31 -0800

    12/12/22
    Added box colliders to trees and buildings fixed animation redid projectile launch and started on cinemachine.

--- RubysAdventureSantibanez/Assets/Scripts/EnemyController.cs
+++ RubysAdventureSantibanez/Assets/Scripts/EnemyController.cs
@@ -9,9 +9,11 @@ public class EnemyController : MonoBehaviour
 
     Rigidbody2D rigidbody2D;
     float timer;
-    
     int direction = 1;
-    private Animator animator;
+    bool broken = true;
+
+    Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +24,12 @@ public class EnemyController : MonoBehaviour
 
     void Update()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won't be executed.
+        if(!broken)
+        {
+            return;
+        }
+
         timer -= Time.deltaTime;
 
         if (timer < 0)
@@ -33,17 +41,23 @@ public class EnemyController : MonoBehaviour
 
     void FixedUpdate()
     {
+        //remember ! inverse the test, so if broken is true !broken will be false and return won’t be executed.
+        if (!broken)
+        {
+            return;
+        }
+
         Vector2 position = rigidbody2D.position;
 
         if (vertical)
         {
-            position.y = position.y + Time.deltaTime * speed * direction; ;
+            position.y = position.y + Time.deltaTime * speed * direction;
             animator.SetFloat("Move X", 0);
-            animator.SetFloat("Move Y", direction); animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
         }
         else
         {
-            position.x = position.x + Time.deltaTime * speed * direction; ;
+            position.x = position.x + Time.deltaTime * speed * direction;
             animator.SetFloat("Move X", direction);
             animator.SetFloat("Move Y", 0);
         }
@@ -59,5 +73,14 @@ public class EnemyController : MonoBehaviour
         {
             player.ChangeHealth(-1);
         }
+    }
+
+    //Public because we want to call it from elsewhere like the projectile script
+    public void Fix()
+    {
+        broken = false;
+        rigidbody2D.simulated = false;
+        //optional if you added the fixed animation
+        animator.SetTrigger("Fixed");
     }
 }
